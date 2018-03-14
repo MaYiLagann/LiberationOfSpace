05efc53bd5321065d7165c06585bd84e30b6aa93
MaYiLagann <lhwshy@naver.com>
2018-03-15 00:13:30 +0900

    프로젝트 시작 및 에셋 추가

---
+++ Liberation of Space/Assets/2. Scripts/RotateSelf.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSelf : MonoBehaviour {
+
+	public Vector3 Direction;
+
+	public float Speed;
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Rotate(Direction * Speed * Time.deltaTime);
+	}
+}
