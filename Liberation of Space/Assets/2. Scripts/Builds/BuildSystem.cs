14aed14795d97c7db7fdc8b9429634523112972d
MaYiLagann <lhwshy@naver.com>
2018-04-25 20:32:09 +0900

    오브젝트 삭제 시스템 추가

--- Liberation of Space/Assets/2. Scripts/Builds/BuildSystem.cs
+++ Liberation of Space/Assets/2. Scripts/Builds/BuildSystem.cs
@@ -17,10 +17,12 @@ public class BuildSystem : MonoBehaviour {
 	private int currentPartId = 0;
 	private List<GameObject> objectParts = new List<GameObject>();
 	private Vector3 latestHitPoint;
+	private GameObject hitObjectPart;
 	private int hitLayer;
 
 	// input system values
 	private bool inputPlace = false;
+	private bool inputRemove = false;
 
 	void Awake () {
 		hitLayer = LayerMask.GetMask(RaycastTargetLayer);
@@ -30,24 +32,39 @@ public class BuildSystem : MonoBehaviour {
 		Raycast((hitpoint) => {
 			PlaceMarkObject.transform.position = hitpoint;
 			latestHitPoint = hitpoint;
-		});
+		}, out hitObjectPart);
+
 		if(Input.GetAxis("Fire1") == 1f) {
-			if(inputPlace) {
+			if(inputPlace) 
 				PlaceObject(latestHitPoint, Vector3.zero);
-			}
 			inputPlace = false;
 		} else 
 			inputPlace = true;
+
+		if(Input.GetAxis("Fire2") == 1f) {
+			if(inputRemove && hitObjectPart != null) 
+				RemoveObject(hitObjectPart);
+			inputRemove = false;
+		} else
+			inputRemove = true;
 	}
 
 	void PlaceObject(Vector3 position, Vector3 rotation) {
-		GameObject newpart = Instantiate(PartObjects[currentPartId]);
+		BuildObjectPart newpart = Instantiate(PartObjects[currentPartId]).AddComponent<BuildObjectPart>();
+		newpart.index = objectParts.Count;
 		newpart.transform.position = position;
 		newpart.transform.rotation = Quaternion.Euler(rotation);
-		objectParts.Add(newpart);
+		objectParts.Add(newpart.gameObject);
 	}
 
-	void Raycast(Action<Vector3> hitFunction) {
+	void RemoveObject(GameObject target) {
+		GameObject obj = objectParts[target.GetComponent<BuildObjectPart>().index];
+		objectParts.RemoveAt(target.GetComponent<BuildObjectPart>().index);
+		Destroy(obj);
+	}
+
+	void Raycast(Action<Vector3> hitFunction, out GameObject hitobject) {
+		hitobject = null;
 		RaycastHit hit;
 		Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
 		if(Physics.Raycast(ray, out hit, Mathf.Infinity, hitLayer)) {
@@ -55,6 +72,11 @@ public class BuildSystem : MonoBehaviour {
 			point.x = Mathf.Round(point.x / GridDistance) * GridDistance;
 			point.y = Mathf.Round(point.y / GridDistance) * GridDistance;
 			point.z = Mathf.Round(point.z / GridDistance) * GridDistance;
+
+			BuildObjectPart part = hit.transform.gameObject.GetComponentInParent<BuildObjectPart>();
+			if(hit.transform.gameObject.layer == LayerMask.NameToLayer(RaycastTargetLayer) && part != null)
+				hitobject = part.transform.gameObject;
+
 			hitFunction(point);
 		}
 	}
